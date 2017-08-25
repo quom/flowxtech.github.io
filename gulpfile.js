f54f01af00dd42009823507d79e5ce5bb5e7c340
Harry Collard <collard41@gmail.com>
2017-08-25 19:16:03 +0100

    Add Harry, make some CSS changes

--- gulpfile.js
+++ gulpfile.js
@@ -104,7 +104,7 @@ gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
-      baseDir: ''
+      baseDir: './'
     },
   })
 })
